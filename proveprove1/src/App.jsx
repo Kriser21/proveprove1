99b968847d682707ab4f363a7fd400d0c66bc431
kriser21 <kristina@nebe.dk>
2020-08-10 12:49:58 +0200

    nyt

--- proveprove1/src/App.jsx
+++ proveprove1/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Nav from './compontens/header/nav';
+import Footer from './compontens/footer/footer';
 
 function App() {
   return (
     <div className="App">
      <Nav/>
+     <Footer/>
     </div>
   );
 }
---
+++ proveprove1/src/compontens/footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Program() {
+    return(
+        <footer>
+            <span className="media">
+                <h2>Hoteller og Destionationer </h2>
+                <i class="fab fa-facebook fa-10x"></i>
+                <i class="fab fa-twitter-square fa-10x"></i>
+            </span>
+            <span className="NewsLetter">
+                <h2>TILMELD NYHEDSBREV</h2>
+            </span>
+        </footer>
+    )
+};
