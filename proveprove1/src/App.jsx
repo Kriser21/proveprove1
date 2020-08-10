041e11c0bd4e89c5f27474175fefc3d5b8d7a930
kriser21 <kristina@nebe.dk>
2020-08-10 10:42:52 +0200

    n

--- proveprove1/src/App.jsx
+++ proveprove1/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import './App.scss';
 
-import Nav from './compontens/header/nav';
-
-
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <Nav />
+     
     </div>
   );
 }
+
+export default App;
--- proveprove1/src/compontens/pages/reservation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Promgram() {
-    return(
-        <section className="promgram">
-            <h2>PROMGRAM</h2>
-        </section>
-    )
-};
