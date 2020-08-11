b1b921bbcbe0d44cd863aaa2e73b762edfa10ec6
kriser21 <kristina@nebe.dk>
2020-08-11 11:53:51 +0200

    n

--- proveprove1/src/compontens/pages/login.jsx
+++ proveprove1/src/compontens/pages/login.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
 
-export default function Praktisk() {
-    return(
-        <div>
-            Praktisk Info
-        </div>
-    )
-};
+import React, { useState, useEffect } from 'react';
+export default function Login() {
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [touched, setTouched] = useState(false);
+  const [valid, setValid] = useState(false);
+  useEffect(() => {
+    setValid(name.length <= 10);
+  });
+  return (
+    <section>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const isValid = touched && valid;
+          if (isValid) {
+            // setName(value);
+            setName('');
+            setPassword('')
+          }
+        }}
+      >
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setTouched(true);
+            setName(e.target.value);
+          }}
+        />
+         <input
+          type="text"
+          value={password}
+          onChange={(e) => {
+            setTouched(true);
+            setPassword(e.target.value);
+          }}
+        />
+      </form>
+      <p>name : {name}</p>
+      <p>password : {password}</p>
+    </section>
+  );
+}
